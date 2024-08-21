67e408e8073719fa8d1631f0df795a4ecbdac0e9
Mlungisi93 <215229827@edu.vut.ac.za>
2024-08-21 06:45:12 +0200

    WORKING

---
+++ packages/bmi/lib/bmi.dart
@@ -0,0 +1,12 @@
+library bmi;
+
+import 'package:intl/intl.dart';
+
+part 'bmi_calculator.dart';
+part 'bmi_formatter.dart';
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
